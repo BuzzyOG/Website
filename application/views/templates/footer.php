521123f980116a28f9a595a8da87f90f9321fe0f
ZeWildGuy <njb3112@iCloud.com>
2014-07-10 15:22:07 +1000

    Added new templates

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,2 +1,6 @@
+	<div class="footer">
+    	<p class="text-muted"><?php $options['footer_content']; ?> Render Time: {elapsed_time}</p>
+    </div>
+</div>
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,106 @@
-<html>
-<head>
-	<title><?php echo $options['website_title'] ?> - <?php echo $title ?></title>
-</head>
-<body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo $title; ?> - <?php echo $options['website_title']; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap-united.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.js"></script>
+
+	<div class="navbar navbar-default navbar-fixed-top">
+	<div class="container">	
+	  <div class="navbar-header">
+	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	      <span class="icon-bar"></span>
+	    </button>
+	    <a class="navbar-brand" href="#"><?php echo $options['website_title'] ?></a>
+	  </div>
+	  <div class="navbar-collapse collapse navbar-responsive-collapse">
+	    <ul class="nav navbar-nav">
+	      <li class="dropdown">
+	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Games<b class="caret"></b></a>
+	        <ul class="dropdown-menu">
+	          <li><a href="#">Game 2</a></li>
+	          <li><a href="#">Game 1</a></li>
+	          <li><a href="#">Game 3</a></li>
+	          <li class="divider"></li>
+	          <li class="dropdown-header">Coming Soon!</li>
+	          <li><a href="#">Game 4</a></li>
+	        </ul>
+	      </li>
+		  <li><a href="#">Players</a></li>
+	    </ul>
+	    <ul class="nav navbar-nav navbar-right">
+	      <li><a href="#">Link</a></li>
+	      <li class="dropdown">
+	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+	        <ul class="dropdown-menu">
+	          <li><a href="#">Action</a></li>
+	          <li><a href="#">Another action</a></li>
+	          <li><a href="#">Something else here</a></li>
+	          <li class="divider"></li>
+	          <li><a href="#">Separated link</a></li>
+	        </ul>
+	      </li>
+	    </ul>
+	  </div>
+	</div>
+	</div>
+	
+	<div class="container" style="padding-top:60px">	
+	<div class="navbar navbar-default">
+		  <div class="navbar-header">
+		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		    </button>
+		    <a class="navbar-brand" href="#"><?php echo $options['website_title'] ?></a>
+		  </div>
+		  <div class="navbar-collapse collapse navbar-responsive-collapse">
+		    <ul class="nav navbar-nav">
+		      <li class="dropdown">
+		        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Games<b class="caret"></b></a>
+		        <ul class="dropdown-menu">
+		          <li><a href="#">Game 2</a></li>
+		          <li><a href="#">Game 1</a></li>
+		          <li><a href="#">Game 3</a></li>
+		          <li class="divider"></li>
+		          <li class="dropdown-header">Coming Soon!</li>
+		          <li><a href="#">Game 4</a></li>
+		        </ul>
+		      </li>
+			  <li><a href="#">Players</a></li>
+		    </ul>
+		    <ul class="nav navbar-nav navbar-right">
+		      <li><a href="#">Link</a></li>
+		      <li class="dropdown">
+		        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+		        <ul class="dropdown-menu">
+		          <li><a href="#">Action</a></li>
+		          <li><a href="#">Another action</a></li>
+		          <li><a href="#">Something else here</a></li>
+		          <li class="divider"></li>
+		          <li><a href="#">Separated link</a></li>
+		        </ul>
+		      </li>
+		    </ul>
+		  </div>
+		</div>
