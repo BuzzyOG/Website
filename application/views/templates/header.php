8ab8c1e9f362e46ddd87e1dffaabb225fddcf3f5
ZeWildGuy <njb3112@iCloud.com>
2014-07-10 15:54:59 +1000

    Major rework of the top navbar to work better on phones

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -23,48 +23,44 @@
     <script src="assets/js/bootstrap.js"></script>
 
 	<div class="navbar navbar-default navbar-fixed-top">
-	<div class="container">	
-	  <div class="navbar-header">
-	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	    </button>
-	    <a class="navbar-brand" href="#"><?php echo $options['website_title'] ?></a>
-	  </div>
-	  <div class="navbar-collapse collapse navbar-responsive-collapse">
-	    <ul class="nav navbar-nav">
-	      <li class="dropdown">
-	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Games<b class="caret"></b></a>
-	        <ul class="dropdown-menu">
-	          <li><a href="#">Game 2</a></li>
-	          <li><a href="#">Game 1</a></li>
-	          <li><a href="#">Game 3</a></li>
-	          <li class="divider"></li>
-	          <li class="dropdown-header">Coming Soon!</li>
-	          <li><a href="#">Game 4</a></li>
-	        </ul>
-	      </li>
-		  <li><a href="#">Players</a></li>
-	    </ul>
-	    <ul class="nav navbar-nav navbar-right">
-	      <li><a href="#">Link</a></li>
-	      <li class="dropdown">
-	        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-	        <ul class="dropdown-menu">
-	          <li><a href="#">Action</a></li>
-	          <li><a href="#">Another action</a></li>
-	          <li><a href="#">Something else here</a></li>
-	          <li class="divider"></li>
-	          <li><a href="#">Separated link</a></li>
-	        </ul>
-	      </li>
-	    </ul>
-	  </div>
-	</div>
+		<div class="container">	
+		 	<div sytle="float:left">
+		 		<a class="navbar-brand" href="#">News Item</a>
+			</div>
+			<ul class="nav navbar-nav" style="float:right">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Username <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+			    		<li><a href="#">Action</a></li>
+			    		<li><a href="#">Another action</a></li>
+			    		<li><a href="#">Something else here</a></li>
+			    		<li class="divider"></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+			<!--
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Link</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Username <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+					    <li><a href="#">Action</a></li>
+					    <li><a href="#">Another action</a></li>
+					    <li><a href="#">Something else here</a></li>
+					    <li class="divider"></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+			-->
+		</div>
 	</div>
 	
-	<div class="container" style="padding-top:60px">	
+	<div class="container" style="padding-top:60px">
+	
+	<p>Logo goes here</p>	
+	
 	<div class="navbar navbar-default">
 		  <div class="navbar-header">
 		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
